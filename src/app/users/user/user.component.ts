46f62f6976c35fa8cc4dd8998dedaaced3f3a747
Pete Russell <pinkbatz@gmail.com>
2017-10-05 14:03:50 +1300

    Adds example of using observables to constantly update parameterized URLs on changes.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,10 +12,24 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    /* See the note below for an explanation why we need both this and the
+       even setup below. */
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+
+    /* Params is an *observable*, so gets called on each async update.
+       Note that in ngOnInit() we're only *setting up* the subscribe function,
+       which means it doesn't get called when ngOnInit() is run. This is why
+       we still need the this.route.snapshot.params above. */
+    this.route.params.subscribe((params: Params) => {
+      this.user = {
+        id: params['id'],
+        name: params['name']
+      }
+    })
   }
 
 }
